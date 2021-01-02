baf783eba61b91b5228a363315dc0e85f172376b
Mayil Vahanan <mavah93@gmail.com>
2021-01-02 21:51:56 +0800

    circuit breaker

---
+++ spring-cloud-circuitbreaker-resilience4j/src/main/java/com/mayil1993/springcloudcircuitbreaker/Controller.java
@@ -0,0 +1,19 @@
+package com.mayil1993.springcloudcircuitbreaker;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @Autowired
+    private AlbumService service;
+
+    @GetMapping("/albums")
+    public String albums() throws Throwable {
+        return service.getAlbumList();
+    }
+
+}
---
+++ spring-cloud-circuitbreaker-resilience4j/src/test/java/com/mayil1993/springcloudcircuitbreaker/SpringCloudCircuitbreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayil1993.springcloudcircuitbreaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudCircuitbreakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
